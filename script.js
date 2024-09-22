566ef24faa7c19ce30a5681a9fee01d42c9aba09
unknown <you@example.com>
2024-09-22 14:49:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    document.getElementById("name").innerHTML = "S";
+}
+
+
+function change(){
+    document.getElementById('demo').innerHTML = "R";
+}
+
+
+
+function myimage() {
+    document.getElementById('myimage').src='image/patha.png'
+   }
+ 
+ function image(){
+     document.getElementById('myimage').src='image/images.png'
+ }
+
+
+
+
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+
+function friend(){
+    document.getElementById('friend-none').style.display="none"
+}
+
+
+function showtext(){ document.getElementById('show').style.display='block'
+}
